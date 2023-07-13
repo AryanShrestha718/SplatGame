818627522d9c0c580fce374892af1af44dd87073
AryanShrestha718 <125922412+AryanShrestha718@users.noreply.github.com>
2023-07-13 19:14:49 -0400

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,91 @@
+class Game {
+  constructor() {
+    this.resetTitle = createElement("h2");
+    this.resetButton = createButton("");
+
+    this.leadeboardTitle = createElement("h2");
+
+    this.leader1 = createElement("h2");
+    this.leader2 = createElement("h2");
+  }
+
+  getState() {
+    var gameStateRef = database.ref("gameState");
+    gameStateRef.on("value", function(data) {
+      gameState = data.val();
+    });
+  }
+  update(state) {
+    database.ref("/").update({
+      gameState: state
+    });
+  }
+
+  start() {
+    player = new Player();
+    playerCount = player.getCount();
+
+    form = new Form();
+    form.display();
+
+   
+  }
+
+  addSprites(spriteGroup, numberOfSprites, spriteImage, scale, positions = []) {
+    for (var i = 0; i < numberOfSprites; i++) {
+      var x, y;
+
+      //C41 //SA
+      if (positions.length > 0) {
+        x = positions[i].x;
+        y = positions[i].y;
+        spriteImage = positions[i].image;
+      } else {
+        x = random(width / 2 + 150, width / 2 - 150);
+        y = random(-height * 4.5, height - 400);
+      }
+      var sprite = createSprite(x, y);
+      sprite.addImage("sprite", spriteImage);
+
+      sprite.scale = scale;
+      spriteGroup.add(sprite);
+    }
+  }
+
+  handleElements() {
+    form.hide();
+    form.titleImg.position(40, 50);
+    form.titleImg.class("gameTitleAfterEffect");
+
+    //C39
+    this.resetTitle.html("Restart Game");
+    this.resetTitle.class("resetText");
+    this.resetTitle.position(width / 2 + 200, 40);
+
+    this.resetButton.class("resetButton");
+    this.resetButton.position(width / 2 + 230, 100);
+
+    this.leadeboardTitle.html("Puntuación");
+    this.leadeboardTitle.class("resetText");
+    this.leadeboardTitle.position(width / 3 - 60, 40);
+
+    this.leader1.class("leadersText");
+    this.leader1.position(width / 3 - 50, 80);
+
+    this.leader2.class("leadersText");
+    this.leader2.position(width / 3 - 50, 130);
+  }
+
+  play() {
+    this.handleElements();
+    this.handleResetButton();
+
+    Player.getPlayersInfo();
+
+      drawSprites();
+    }
+  
+  end() {
+    console.log("End of Game");
+  }
+}
